73e20fcb60c9506b218383b92ae9c1b3acbaa07d
mohsen.ameli@cluep.com <mohsen.ameli83@gmail.com>
2025-03-16 19:57:55 -0400

    Added panels and layouts for login and register pages

--- src/App.java
+++ src/App.java
@@ -1,68 +1,127 @@
 import java.io.*;
+import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-class App {
+public class App {
+    private JFrame frame;
+    private JPanel mainPanel;
+    private CardLayout cardLayout;
+
     public static void main(String[] args) {
-        // Creating instance of JFrame
-        JFrame frame = new JFrame();
+        App a = new App();
+    }
+
+    public void registerPage() {
+        JPanel register = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
 
         JLabel firstNameLabel = new JLabel("First Name");
+        JTextField firstName = new JTextField(15);
+
         JLabel lastNameLabel = new JLabel("Last Name");
+        JTextField lastName = new JTextField(15);
+
         JLabel usernameLabel = new JLabel("Username");
+        JTextField username = new JTextField(15);
+
         JLabel emailLabel = new JLabel("Email");
+        JTextField email = new JTextField(15);
+
         JLabel passwordLabel = new JLabel("Password");
-        firstNameLabel.setBounds(0, 0, 200, 20);
-        lastNameLabel.setBounds(0, 40, 200, 20);
-        usernameLabel.setBounds(0, 80, 200, 20);
-        emailLabel.setBounds(0, 120, 200, 20);
-        passwordLabel.setBounds(0, 160, 200, 20);
+        JTextField password = new JTextField(15);
 
-        JTextField firstName = new JTextField();
-        JTextField lastName = new JTextField();
-        JTextField username = new JTextField();
-        JTextField email = new JTextField();
-        JTextField password = new JTextField();
-        firstName.setBounds(0, 20, 200, 20);
-        lastName.setBounds(0, 60, 200, 20);
-        username.setBounds(0, 100, 200, 20);
-        email.setBounds(0, 140, 200, 20);
-        password.setBounds(0, 180, 200, 20);
+        JButton registerButton = new JButton("Sign Up");
+        JButton goToLogin = new JButton("Already have an account? Login");
+        goToLogin.addActionListener(e -> cardLayout.show(mainPanel, "login"));
 
-        JButton signUp = new JButton("Sign Up!");
-        JButton goLogin = new JButton("Go to Login!");
-        signUp.setBounds(50, 220, 100, 50);
-        goLogin.setBounds(200, 220, 125, 50);
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        register.add(firstNameLabel, gbc);
+        gbc.gridx = 1;
+        register.add(firstName, gbc);
 
-        signUp.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                System.out.println(firstName.getText());
-            }
-        });
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        register.add(lastNameLabel, gbc);
+        gbc.gridx = 1;
+        register.add(lastName, gbc);
 
-        // adding button in JFrame
-        frame.add(firstNameLabel);
-        frame.add(lastNameLabel);
-        frame.add(usernameLabel);
-        frame.add(emailLabel);
-        frame.add(passwordLabel);
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        register.add(usernameLabel, gbc);
+        gbc.gridx = 1;
+        register.add(username, gbc);
 
-        frame.add(firstName);
-        frame.add(lastName);
-        frame.add(username);
-        frame.add(email);
-        frame.add(password);
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        register.add(emailLabel, gbc);
+        gbc.gridx = 1;
+        register.add(email, gbc);
 
-        frame.add(signUp);
-        frame.add(goLogin);
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        register.add(passwordLabel, gbc);
+        gbc.gridx = 1;
+        register.add(password, gbc);
 
-        // 400 width and 500 height
+        gbc.gridx = 0;
+        gbc.gridy = 5;
+        register.add(registerButton, gbc);
+        gbc.gridx = 1;
+        register.add(goToLogin, gbc);
+
+        mainPanel.add(register, "register");
+    }
+
+    public void loginPage() {
+        JPanel login = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+
+        JLabel userLabel = new JLabel("Username:");
+        JTextField userField = new JTextField(15);
+
+        JLabel passLabel = new JLabel("Password:");
+        JPasswordField passField = new JPasswordField(15);
+
+        JButton loginButton = new JButton("Login");
+        JButton registerButton = new JButton("Don't have an account? Register");
+        registerButton.addActionListener(e -> cardLayout.show(mainPanel, "register"));
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        login.add(userLabel, gbc);
+        gbc.gridx = 1;
+        login.add(userField, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        login.add(passLabel, gbc);
+        gbc.gridx = 1;
+        login.add(passField, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        login.add(loginButton, gbc);
+        gbc.gridx = 1;
+        login.add(registerButton, gbc);
+
+        mainPanel.add(login, "login");
+    }
+
+    public App() {
+        frame = new JFrame("YorkU Parking System");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        cardLayout = new CardLayout();
+        mainPanel = new JPanel(cardLayout);
+
+        loginPage();
+        registerPage();
+
+        frame.add(mainPanel);
         frame.setSize(500, 600);
-
-        // using no layout managers
-        frame.setLayout(null);
-
-        // making the frame visible
         frame.setVisible(true);
     }
 }
